3f41e61b52c8eeb29ec9b344202a5f3b8135dc62
miguelaz7 <miguelangelo071105@gmail.com>
2026-04-04 15:00:37 +0100

    docker files

--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -146,15 +146,6 @@ function TicketIcon() {
   );
 }
 
-function BellIcon() {
-  return (
-    <svg viewBox="0 0 24 24" width="22" height="22" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-      <path d="M18 8A6 6 0 0 0 6 8c0 7-3 9-3 9h18s-3-2-3-9"></path>
-      <path d="M13.73 21a2 2 0 0 1-3.46 0"></path>
-    </svg>
-  );
-}
-
 function MapIcon() {
   return (
     <svg viewBox="0 0 24 24" width="22" height="22" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
